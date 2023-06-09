17baf8079c72f7079f5230cc8dc7a160f33678f3
Your Name <minhthuc1999qn@gmail.com>
2023-06-09 16:36:39 +0700

    update code

---
+++ src/components/Results.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+function Results({ totalCorrect, totalQuestion }) {
+  return (
+    <div className="final-results">
+      <h1>Final Results</h1>
+      {totalCorrect === 6 ||
+      totalCorrect === 7 ||
+      totalCorrect === 8 ||
+      totalCorrect === 9 ||
+      totalCorrect === 10 ? (
+        <div>
+          <p style={{ width: "20%", height: "20%", display: "inline-block" }}>
+            <img
+              src="https://cdn-icons-png.flaticon.com/256/7880/7880191.png"
+              alt=""
+            />
+          </p>
+          <p>Congratulations!!</p>
+          <p>You are amazing!!</p>
+        </div>
+      ) : (
+        <div>
+          <p style={{ width: "20%", height: "20%", display: "inline-block" }}>
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/560/560514.png"
+              alt=""
+            />
+          </p>
+          <p>Completed!!</p>
+          <p>Better luck next time!!</p>
+        </div>
+      )}
+
+      <h2>
+        {totalCorrect} out of {totalQuestion} correct - (
+        {(totalCorrect / totalQuestion) * 100}%)
+      </h2>
+      <button>Restart game</button>
+    </div>
+  );
+}
+
+export default Results;
